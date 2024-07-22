4dfa3642e128d8ae3dfb0093eb56ad0500a467e6
UtsavRam <137594499+UtsavRam@users.noreply.github.com>
2024-07-22 22:31:27 +0530

    22-07-2024
    Redux_Crud

---
+++ Task/Redux_Crud/src/Data.js
@@ -0,0 +1,12 @@
+export const userList = [
+    {
+        "name":"Utsav",
+        "email":"U@gmail.com",
+        "id":1
+    },
+    {
+        "name":"Xyz",
+        "email":"xyz@gmail.com",
+        "id":2
+    }
+]
